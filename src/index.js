5ab4362d007860094161d1963a0c59c6f7bd72e7
kylanhurt <kylan.hurt@gmail.com>
2019-02-01 15:59:24 -0800

    Add HERC's exchange

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 export { coinbasePlugin } from './coinbase.js'
 export { shapeshiftPlugin } from './shapeshift.js'
 export { coincapPlugin } from './coincap.js'
+export { hercPlugin } from './herc.js'
 export { currencyconverterapiPlugin } from './currencyconverterapi.js'
