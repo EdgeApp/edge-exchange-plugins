52a78362682814943932789976e6914f482c41e4
kylanhurt <kylan.hurt@gmail.com>
2019-07-09 19:28:29 -0700

    Enable HERC and AGLD exchange rates

--- src/index.js
+++ src/index.js
@@ -4,14 +4,13 @@ import { makeCoinbasePlugin } from './rate/coinbase.js'
 import { makeCoincapPlugin } from './rate/coincap.js'
 import { makeCoincapLegacyPlugin } from './rate/coincapLegacy.js'
 import { makeCurrencyconverterapiPlugin } from './rate/currencyconverterapi.js'
-import { makeHercPlugin } from './rate/herc.js'
 import { makeNomicsPlugin } from './rate/nomics.js'
 import { makeShapeshiftRatePlugin } from './rate/shapeshift-rate.js'
+import { makeXagauPlugin } from './rate/xagau.js'
 import { makeChangellyPlugin } from './swap/changelly.js'
 import { makeChangeNowPlugin } from './swap/changenow.js'
 import { makeFaastPlugin } from './swap/faast.js'
 import { makeShapeshiftPlugin } from './swap/shapeshift.js'
-import { makeTotlePlugin } from './plugins/totle.js'
 
 const edgeCorePlugins = {
   // Rate plugins:
@@ -20,15 +19,13 @@ const edgeCorePlugins = {
   coincap: makeCoincapPlugin,
   coincapLegacy: makeCoincapLegacyPlugin,
   currencyconverterapi: makeCurrencyconverterapiPlugin,
-  herc: makeHercPlugin,
   nomics: makeNomicsPlugin,
-
+  xagau: makeXagauPlugin,
   // Swap plugins:
   changelly: makeChangellyPlugin,
   changenow: makeChangeNowPlugin,
   faast: makeFaastPlugin,
-  shapeshift: makeShapeshiftPlugin,
-  totle: makeTotlePlugin
+  shapeshift: makeShapeshiftPlugin
 }
 
 if (
