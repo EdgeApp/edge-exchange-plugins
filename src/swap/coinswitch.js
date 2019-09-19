e1a07358b20493c9812f14981b2b86e53e32d4f3
ramendra <ramendra.singh@coinswitch.co>
2019-09-19 08:45:22 +0000

    coinswitch fixed rate
    coinswitch fixed rate
    
    detect deposit till 15min
    
    Implement Coinswitch fixed rate

--- src/swap/coinswitch.js
+++ src/swap/coinswitch.js
@@ -24,7 +24,9 @@ const swapInfo = {
 }
 
 const uri = 'https://api.coinswitch.co/'
-const expirationMs = 60 * 60 * 12 * 1000
+const expirationMs = 1000 * 60 * 15
+const fixedExpirationMs = 1000 * 60 * 5
+
 
 type QuoteInfo = {
   orderId: string,
@@ -87,148 +89,279 @@ export function makeCoinSwitchPlugin(
       throw new Error(`CoinSwitch returned error code ${reply.status}`)
     }
     const out = await reply.json
-    io.console.info('coinswitch fixed reply:', out)
+    io.console.info('coinswitch reply:', out)
     return out
   }
 
   const out: EdgeSwapPlugin = {
     swapInfo,
 
-    async fetchSwapQuote(
-      request: EdgeSwapRequest,
-      userSettings: Object | void
-    ): Promise<EdgeSwapPluginQuote> {
-      // Grab addresses:
-      const [fromAddress, toAddress] = await Promise.all([
-        getAddress(request.fromWallet, request.fromCurrencyCode),
-        getAddress(request.toWallet, request.toCurrencyCode)
-      ])
-
-      const quoteAmount =
-        request.quoteFor === 'from'
-          ? await request.fromWallet.nativeToDenomination(
-              request.nativeAmount,
-              request.fromCurrencyCode
-            )
-          : await request.toWallet.nativeToDenomination(
-              request.nativeAmount,
-              request.toCurrencyCode
-            )
-
-      const quoteParams =
-        request.quoteFor === 'from'
-          ? {
-              depositCoin: request.fromCurrencyCode,
-              destinationCoin: request.toCurrencyCode,
-              depositCoinAmount: quoteAmount
-            }
-          : {
-              depositCoin: request.fromCurrencyCode,
-              destinationCoin: request.toCurrencyCode,
-              destinationCoinAmount: quoteAmount
-            }
-
-      const quoteReplies = await Promise.all([
-        call({
-          route: 'v2/rate',
-          params: {
-            depositCoin: quoteParams.depositCoin.toLowerCase(),
-            destinationCoin: quoteParams.destinationCoin.toLowerCase()
-          }
-        })
-      ])
-
-      checkReply(quoteReplies[0], request)
-
-      let fromAmount, fromNativeAmount, toNativeAmount
-      const minerFee = quoteReplies[0].data.minerFee.toString()
-      const rate = quoteReplies[0].data.rate.toString()
-
-      if (request.quoteFor === 'from') {
-        fromAmount = quoteAmount
-        fromNativeAmount = request.nativeAmount
-        const exchangeAmountBeforeMinerFee = mul(rate, quoteAmount)
-        const exchangeAmount = sub(exchangeAmountBeforeMinerFee, minerFee)
-        toNativeAmount = await request.toWallet.denominationToNative(
-          exchangeAmount,
-          request.toCurrencyCode
-        )
-      } else {
-        const exchangeAmountAfterMinerFee = add(quoteAmount, minerFee)
-        fromAmount = div(exchangeAmountAfterMinerFee, rate, 16)
-
-        fromNativeAmount = await request.fromWallet.denominationToNative(
-          fromAmount,
-          request.fromCurrencyCode
-        )
-        toNativeAmount = request.nativeAmount
+    async fetchSwapQuote (request: EdgeSwapRequest, userSettings: Object | void): Promise<EdgeSwapPluginQuote> {
+      const fixedPromise = this.getFixedQuote(request, userSettings)
+      const estimatePromise = this.getEstimate(request, userSettings)
+      // try fixed and if error then get estimate
+      try {
+        const fixedResult = await fixedPromise
+        return fixedResult
+      } catch (e) {
+        const estimateResult = await estimatePromise
+        return estimateResult
       }
+      },
+      async getFixedQuote (
+            request: EdgeSwapRequest,
+            userSettings: Object | void
+          ): Promise<EdgeSwapPluginQuote> {
+             const [fromAddress, toAddress] = await Promise.all([
+                  getAddress(request.fromWallet, request.fromCurrencyCode),
+                  getAddress(request.toWallet, request.toCurrencyCode)
+                ])
 
-      const [nativeMin, nativeMax] = await Promise.all([
-        request.fromWallet.denominationToNative(
-          quoteReplies[0].data.limitMinDepositCoin.toString(),
-          request.fromCurrencyCode
-        ),
-        request.fromWallet.denominationToNative(
-          quoteReplies[0].data.limitMaxDepositCoin.toString(),
-          request.fromCurrencyCode
-        )
-      ])
+                const quoteAmount =
+                  request.quoteFor === 'from'
+                    ? await request.fromWallet.nativeToDenomination(request.nativeAmount, request.fromCurrencyCode)
+                    : await request.toWallet.nativeToDenomination(request.nativeAmount, request.toCurrencyCode)
 
-      if (lt(fromNativeAmount, nativeMin)) {
-        throw new SwapBelowLimitError(swapInfo, nativeMin)
-      }
+                const quoteParams =
+                  request.quoteFor === 'from'
+                    ? {
+                      depositCoin: request.fromCurrencyCode.toLowerCase(),
+                      destinationCoin: request.toCurrencyCode.toLowerCase(),
+                      depositCoinAmount: quoteAmount
+                    }
+                    : {
+                      depositCoin: request.fromCurrencyCode.toLowerCase(),
+                      destinationCoin: request.toCurrencyCode.toLowerCase(),
+                      destinationCoinAmount: quoteAmount
+                    }
 
-      if (gt(fromNativeAmount, nativeMax)) {
-        throw new SwapAboveLimitError(swapInfo, nativeMax)
-      }
 
-      const createOrder = await call({
-        route: 'v2/order',
-        params: {
-          depositCoin: quoteParams.depositCoin.toLowerCase(),
-          destinationCoin: quoteParams.destinationCoin.toLowerCase(),
-          depositCoinAmount: parseFloat(fromAmount),
-          destinationAddress: { address: toAddress, tag: null },
-          refundAddress: { address: fromAddress, tag: null }
-        }
-      })
+                const quoteReplies = await Promise.all([
+                  call({
+                    route: 'v2/fixed/offer',
+                    params: quoteParams
+                  }),
+                  call({
+                    route: 'v2/fixed/pairs',
+                    params: {
+                      depositCoin: quoteParams.depositCoin,
+                      destinationCoin: quoteParams.destinationCoin
+                    }
+                  })
+                ])
 
-      checkReply(createOrder)
-      const quoteInfo: QuoteInfo = createOrder.data
+                checkReply(quoteReplies[0], request)
+                checkReply(quoteReplies[1], request)
 
-      // Make the transaction:
-      const spendInfo = {
-        currencyCode: request.fromCurrencyCode,
-        spendTargets: [
-          {
-            nativeAmount: fromNativeAmount,
-            publicAddress: quoteInfo.exchangeAddress.address,
-            otherParams: {
-              uniqueIdentifier: quoteInfo.exchangeAddress.tag
-            }
-          }
-        ]
-      }
-      io.console.info('coinswitch spendInfo', spendInfo)
-      const tx: EdgeTransaction = await request.fromWallet.makeSpend(spendInfo)
-      if (!tx.otherParams) tx.otherParams = {}
-      tx.otherParams.payinAddress = spendInfo.spendTargets[0].publicAddress
-      tx.otherParams.uniqueIdentifier =
-        spendInfo.spendTargets[0].otherParams.uniqueIdentifier
+                let fromAmount, fromNativeAmount, toNativeAmount
+                const offerReferenceId = quoteReplies[0].data.offerReferenceId
 
-      return makeSwapPluginQuote(
-        request,
-        fromNativeAmount,
-        toNativeAmount,
-        tx,
-        toAddress,
-        'coinswitch',
-        true,
-        new Date(Date.now() + expirationMs),
-        quoteInfo.orderId
-      )
-    }
+                if (request.quoteFor === 'from') {
+                  fromAmount = quoteAmount
+                  fromNativeAmount = request.nativeAmount
+                  const exchangeAmount = quoteReplies[0].data.destinationCoinAmount
+                  toNativeAmount = await request.toWallet.denominationToNative(exchangeAmount, request.toCurrencyCode)
+                } else {
+                  fromAmount = quoteReplies[0].data.depositCoinAmount
+                  fromNativeAmount = await request.fromWallet.denominationToNative(fromAmount, request.fromCurrencyCode)
+                  toNativeAmount = request.nativeAmount
+                }
+
+                const [nativeMin, nativeMax] = await Promise.all([
+                  request.fromWallet.denominationToNative(quoteReplies[1].data[0].limitMinDepositCoin.toString(), request.fromCurrencyCode),
+                  request.fromWallet.denominationToNative(quoteReplies[1].data[0].limitMaxDepositCoin.toString(), request.fromCurrencyCode)
+                ])
+
+                if (lt(fromNativeAmount, nativeMin)) {
+                  throw new SwapBelowLimitError(swapInfo, nativeMin)
+                }
+
+                if (gt(fromNativeAmount, nativeMax)) {
+                  throw new SwapAboveLimitError(swapInfo, nativeMax)
+                }
+
+                const createOrder = await call({
+                  route: 'v2/fixed/order',
+                  params: {
+                    depositCoin: quoteParams.depositCoin.toLowerCase(),
+                    destinationCoin: quoteParams.destinationCoin.toLowerCase(),
+                    depositCoinAmount: parseFloat(fromAmount),
+                    offerReferenceId: offerReferenceId,
+                    destinationAddress: { address: toAddress, tag: null },
+                    refundAddress: { address: fromAddress, tag: null }
+                  }
+                })
+
+                checkReply(createOrder)
+                const quoteInfo: QuoteInfo = createOrder.data
+
+                // Make the transaction:
+                const spendInfo = {
+                  currencyCode: request.fromCurrencyCode,
+                  spendTargets: [
+                    {
+                      nativeAmount: fromNativeAmount,
+                      publicAddress: quoteInfo.exchangeAddress.address,
+                      otherParams: {
+                        uniqueIdentifier: quoteInfo.exchangeAddress.tag
+                      }
+                    }
+                  ]
+                }
+                io.console.info('coinswitch fixedRate spendInfo', spendInfo)
+                const tx: EdgeTransaction = await request.fromWallet.makeSpend(spendInfo)
+                if (!tx.otherParams) tx.otherParams = {}
+                tx.otherParams.payinAddress = spendInfo.spendTargets[0].publicAddress
+                tx.otherParams.uniqueIdentifier = spendInfo.spendTargets[0].otherParams.uniqueIdentifier
+
+                return makeSwapPluginQuote(
+                  request,
+                  fromNativeAmount,
+                  toNativeAmount,
+                  tx,
+                  toAddress,
+                  'coinswitch',
+                  false, // isEstimate
+                  new Date(Date.now() + fixedExpirationMs),
+                  quoteInfo.orderId
+                )
+               },
+                 async getEstimate (
+                      request: EdgeSwapRequest,
+                      userSettings: Object | void
+                    ): Promise<EdgeSwapPluginQuote> {
+                  const [fromAddress, toAddress] = await Promise.all([
+                      getAddress(request.fromWallet, request.fromCurrencyCode),
+                      getAddress(request.toWallet, request.toCurrencyCode)
+                    ])
+
+                    const quoteAmount =
+                      request.quoteFor === 'from'
+                        ? await request.fromWallet.nativeToDenomination(
+                          request.nativeAmount,
+                          request.fromCurrencyCode
+                        )
+                        : await request.toWallet.nativeToDenomination(
+                          request.nativeAmount,
+                          request.toCurrencyCode
+                        )
+
+                    const quoteParams =
+                      request.quoteFor === 'from'
+                        ? {
+                          depositCoin: request.fromCurrencyCode.toLowerCase(),
+                          destinationCoin: request.toCurrencyCode.toLowerCase(),
+                          depositCoinAmount: quoteAmount
+                        }
+                        : {
+                          depositCoin: request.fromCurrencyCode.toLowerCase(),
+                          destinationCoin: request.toCurrencyCode.toLowerCase(),
+                          destinationCoinAmount: quoteAmount
+                        }
+
+                    const quoteReplies = await Promise.all([
+                      call({
+                        route: 'v2/rate',
+                        params: {
+                          depositCoin: quoteParams.depositCoin.toLowerCase(),
+                          destinationCoin: quoteParams.destinationCoin.toLowerCase()
+                        }
+                      })
+                    ])
+
+                    checkReply(quoteReplies[0], request)
+
+                    let fromAmount, fromNativeAmount, toNativeAmount
+                    const minerFee = quoteReplies[0].data.minerFee.toString()
+                    const rate = quoteReplies[0].data.rate.toString()
+
+                    if (request.quoteFor === 'from') {
+                      fromAmount = quoteAmount
+                      fromNativeAmount = request.nativeAmount
+                      const exchangeAmountBeforeMinerFee = mul(rate, quoteAmount)
+                      const exchangeAmount = sub(exchangeAmountBeforeMinerFee, minerFee)
+                      toNativeAmount = await request.toWallet.denominationToNative(
+                        exchangeAmount,
+                        request.toCurrencyCode
+                      )
+                    } else {
+                      const exchangeAmountAfterMinerFee = add(quoteAmount, minerFee)
+                      fromAmount = div(exchangeAmountAfterMinerFee, rate, 16)
+
+                      fromNativeAmount = await request.fromWallet.denominationToNative(
+                        fromAmount,
+                        request.fromCurrencyCode
+                      )
+                      toNativeAmount = request.nativeAmount
+                    }
+
+                    const [nativeMin, nativeMax] = await Promise.all([
+                      request.fromWallet.denominationToNative(
+                        quoteReplies[0].data.limitMinDepositCoin.toString(),
+                        request.fromCurrencyCode
+                      ),
+                      request.fromWallet.denominationToNative(
+                        quoteReplies[0].data.limitMaxDepositCoin.toString(),
+                        request.fromCurrencyCode
+                      )
+                    ])
+
+                    if (lt(fromNativeAmount, nativeMin)) {
+                      throw new SwapBelowLimitError(swapInfo, nativeMin)
+                    }
+
+                    if (gt(fromNativeAmount, nativeMax)) {
+                      throw new SwapAboveLimitError(swapInfo, nativeMax)
+                    }
+
+                    const createOrder = await call({
+                      route: 'v2/order',
+                      params: {
+                        depositCoin: quoteParams.depositCoin.toLowerCase(),
+                        destinationCoin: quoteParams.destinationCoin.toLowerCase(),
+                        depositCoinAmount: parseFloat(fromAmount),
+                        destinationAddress: { address: toAddress, tag: null },
+                        refundAddress: { address: fromAddress, tag: null }
+                      }
+                    })
+
+                    checkReply(createOrder)
+                    const quoteInfo: QuoteInfo = createOrder.data
+
+                    // Make the transaction:
+                    const spendInfo = {
+                      currencyCode: request.fromCurrencyCode,
+                      spendTargets: [
+                        {
+                          nativeAmount: fromNativeAmount,
+                          publicAddress: quoteInfo.exchangeAddress.address,
+                          otherParams: {
+                            uniqueIdentifier: quoteInfo.exchangeAddress.tag
+                          }
+                        }
+                      ]
+                    }
+                    io.console.info('coinswitch estimate spendInfo', spendInfo)
+                    const tx: EdgeTransaction = await request.fromWallet.makeSpend(spendInfo)
+                    if (!tx.otherParams) tx.otherParams = {}
+                    tx.otherParams.payinAddress = spendInfo.spendTargets[0].publicAddress
+                    tx.otherParams.uniqueIdentifier =
+                      spendInfo.spendTargets[0].otherParams.uniqueIdentifier
+
+                    return makeSwapPluginQuote(
+                      request,
+                      fromNativeAmount,
+                      toNativeAmount,
+                      tx,
+                      toAddress,
+                      'coinswitch',
+                      true,
+                      new Date(Date.now() + expirationMs),
+                      quoteInfo.orderId
+                    )
+                  }
+
+
   }
 
   return out
