a929b1f429261221fa4b3ca116640481adda3f00
Matthew <matthew@edge.app>
2020-04-02 18:43:14 -0700

    Temporary $0 FIO fixed rate.

--- src/rate/constantRate.js
+++ src/rate/constantRate.js
@@ -35,6 +35,11 @@ export function makeConstantRatePlugin(
           fromCurrency: 'WBTC',
           toCurrency: 'BTC',
           rate: 1
+        },
+        {
+          fromCurrency: 'iso:USD',
+          toCurrency: 'FIO',
+          rate: 0
         }
       ]
       return pairs
