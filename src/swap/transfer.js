385c1996d102de6b351ca75f880e64d8c19c407d
Noah Passalacqua <noah.passa@gmail.com>
2020-04-21 16:40:44 -0500

    Create 1:1 transfer plugin.

---
+++ src/swap/transfer.js
@@ -0,0 +1,72 @@
+// @flow
+
+import {
+  type EdgeCorePluginOptions,
+  type EdgeSwapInfo,
+  type EdgeSwapPlugin,
+  type EdgeSwapQuote,
+  type EdgeSwapRequest,
+  type EdgeTransaction,
+  SwapCurrencyError
+} from 'edge-core-js/types'
+
+import { makeSwapPluginQuote } from '../swap-helpers.js'
+
+const pluginId = 'transfer'
+const swapInfo: EdgeSwapInfo = {
+  pluginId,
+  displayName: 'Transfer',
+
+  quoteUri: undefined,
+  supportEmail: 'support@edge.com'
+}
+
+export function makeTransferPlugin(
+  opts: EdgeCorePluginOptions
+): EdgeSwapPlugin {
+  const { log } = opts
+
+  const out: EdgeSwapPlugin = {
+    swapInfo,
+
+    async fetchSwapQuote(
+      request: EdgeSwapRequest,
+      userSettings: Object | void
+    ): Promise<EdgeSwapQuote> {
+      if (request.fromCurrencyCode !== request.toCurrencyCode) {
+        throw new SwapCurrencyError(
+          swapInfo,
+          request.fromCurrencyCode,
+          request.toCurrencyCode
+        )
+      }
+
+      const {
+        publicAddress: toAddress
+      } = await request.toWallet.getReceiveAddress()
+
+      const tx: EdgeTransaction = await request.fromWallet.makeSpend({
+        currencyCode: request.fromCurrencyCode,
+        spendTargets: [
+          {
+            nativeAmount: request.nativeAmount,
+            publicAddress: toAddress
+          }
+        ]
+      })
+
+      const quote = makeSwapPluginQuote(
+        request,
+        request.nativeAmount,
+        request.nativeAmount,
+        tx,
+        toAddress,
+        'transfer'
+      )
+      log(quote)
+      return quote
+    }
+  }
+
+  return out
+}
