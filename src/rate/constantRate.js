1bda83cfaff1896dac090f2ce6aa34dd238e7332
Matthew <matthew@edge.app>
2021-01-25 22:38:50 -0800

    Move aTokens into constantRate plugin

--- src/rate/constantRate.js
+++ src/rate/constantRate.js
@@ -36,6 +36,86 @@ export function makeConstantRatePlugin(
           fromCurrency: 'WBTC',
           toCurrency: 'BTC',
           rate: 1
+        },
+        {
+          fromCurrency: 'AYFI',
+          toCurrency: 'YFI',
+          rate: 1
+        },
+        {
+          fromCurrency: 'ALINK',
+          toCurrency: 'LINK',
+          rate: 1
+        },
+        {
+          fromCurrency: 'ADAI',
+          toCurrency: 'DAI',
+          rate: 1
+        },
+        {
+          fromCurrency: 'ABAT',
+          toCurrency: 'BAT',
+          rate: 1
+        },
+        {
+          fromCurrency: 'AWETH',
+          toCurrency: 'WETH',
+          rate: 1
+        },
+        {
+          fromCurrency: 'AWBTC',
+          toCurrency: 'WBTC',
+          rate: 1
+        },
+        {
+          fromCurrency: 'ASNX',
+          toCurrency: 'SNX',
+          rate: 1
+        },
+        {
+          fromCurrency: 'AREN',
+          toCurrency: 'REN',
+          rate: 1
+        },
+        {
+          fromCurrency: 'AUSDT',
+          toCurrency: 'USDT',
+          rate: 1
+        },
+        {
+          fromCurrency: 'AMKR',
+          toCurrency: 'MKR',
+          rate: 1
+        },
+        {
+          fromCurrency: 'AMANA',
+          toCurrency: 'MANA',
+          rate: 1
+        },
+        {
+          fromCurrency: 'AZRX',
+          toCurrency: 'ZRX',
+          rate: 1
+        },
+        {
+          fromCurrency: 'AKNC',
+          toCurrency: 'KNC',
+          rate: 1
+        },
+        {
+          fromCurrency: 'AUSDC',
+          toCurrency: 'USDC',
+          rate: 1
+        },
+        {
+          fromCurrency: 'ASUSD',
+          toCurrency: 'SUSD',
+          rate: 1
+        },
+        {
+          fromCurrency: 'AUNI',
+          toCurrency: 'UNI',
+          rate: 1
         }
       ]
       return pairs
