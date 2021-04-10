dbac3bc7859cbb3d8185c0099f420613368143dc
Matthew <matthew@edge.app>
2021-04-09 17:45:34 -0700

    Bulkify Nomics query
    In the process I hardcoded the convert parameter to USD.. Leaving it as a variable and coding for it would result in hundred of queries on new installations.

--- src/rate/nomics.js
+++ src/rate/nomics.js
@@ -1,6 +1,6 @@
 // @flow
 
-import { asArray, asObject, asString } from 'cleaners'
+import { asArray, asObject, asOptional, asString } from 'cleaners'
 import {
   type EdgeCorePluginOptions,
   type EdgeRatePlugin
@@ -8,14 +8,10 @@ import {
 
 const asNomicsResponse = asArray(
   asObject({
-    price: asString
+    price: asOptional(asString),
+    symbol: asString
   })
 )
-
-function checkIfFiat(code: string): boolean {
-  if (code.indexOf('iso:') >= 0) return true
-  return false
-}
 
 export function makeNomicsPlugin(opts: EdgeCorePluginOptions): EdgeRatePlugin {
   const { io, initOptions, log } = opts
@@ -33,29 +29,40 @@ export function makeNomicsPlugin(opts: EdgeCorePluginOptions): EdgeRatePlugin {
 
     async fetchRates(pairsHint) {
       const pairs = []
-      for (const pair of pairsHint) {
-        // Skip if codes are both fiat or crypto
-        if (
-          (checkIfFiat(pair.fromCurrency) && checkIfFiat(pair.toCurrency)) ||
-          (!checkIfFiat(pair.fromCurrency) && !checkIfFiat(pair.toCurrency))
-        )
-          continue
-        const fiatCode = pair.toCurrency.split(':')
+
+      // Create query strings
+      const queryStrings = []
+      let filteredPairs = []
+      for (let i = 0; i < pairsHint.length; i++) {
+        if (pairsHint[i].fromCurrency.indexOf('iso:') >= 0) continue
+        if (filteredPairs.some(cc => cc === pairsHint[i].fromCurrency)) continue
+        filteredPairs.push(pairsHint[i].fromCurrency)
+        if (filteredPairs.length === 100 || i === pairsHint.length - 1) {
+          queryStrings.push(filteredPairs.join(','))
+          filteredPairs = []
+        }
+      }
+
+      for (const query of queryStrings) {
         try {
           const reply = await fetchCors(
-            `https://api.nomics.com/v1/currencies/ticker?key=${apiKey}&ids=${pair.fromCurrency}&convert=${fiatCode[1]}`
+            `https://api.nomics.com/v1/currencies/ticker?key=${apiKey}&ids=${query}&convert=USD`
           )
           if (reply.status === 429) continue
-          const jsonData = await reply.json()
-          const rate = Number(asNomicsResponse(jsonData)[0].price)
-          pairs.push({
-            fromCurrency: pair.fromCurrency,
-            toCurrency: pair.toCurrency,
-            rate
+          asNomicsResponse(await reply.json()).forEach(rate => {
+            // When Nomics considers a coin "dead" they don't return a price
+            if (rate.price)
+              pairs.push({
+                fromCurrency: rate.symbol,
+                toCurrency: 'iso:USD',
+                rate: Number(rate.price)
+              })
           })
         } catch (e) {
           log.warn(
-            `Issue with Nomics rate data structure for ${pair.fromCurrency}/${pair.toCurrency} pair. Error: ${e}`
+            `Issue with Nomics rate data structure. Querystrings ${JSON.stringify(
+              queryStrings
+            )} Error: ${e}`
           )
         }
       }
