d465395cdb133a1df20b9b2395f90ead69b0e089
Matthew <matthew@edge.app>
2021-04-15 16:59:09 -0700

    Update EdgeRates plugin to query 100 currencies at a time

--- src/rate/edgeRates.js
+++ src/rate/edgeRates.js
@@ -1,13 +1,43 @@
 // @flow
 
-import { asObject, asString } from 'cleaners'
+import { asArray, asObject, asOptional, asString } from 'cleaners'
 import {
   type EdgeCorePluginOptions,
   type EdgeRatePlugin
 } from 'edge-core-js/types'
 
+const asEdgeRatesExchangeRate = asObject({
+  data: asObject({
+    currency_pair: asString,
+    date: asOptional(asString),
+    exchangeRate: asOptional(asString),
+    errors: asOptional(
+      asObject({
+        name: asString,
+        message: asString,
+        stack: asOptional(asString)
+      })
+    )
+  })
+})
+
+// const asEdgeRatesExchangeRate = asObject({
+//   data: asObject({
+//     currency_pair: asOptional(asString),
+//     date: asOptional(asString),
+//     exchangeRate: asOptional(asString),
+//     errors: asOptional(
+//       asObject({
+//         name: asString,
+//         message: asString,
+//         stack: asOptional(asString)
+//       })
+//     )
+//   })
+// })
+
 const asEdgeRatesResponse = asObject({
-  exchangeRate: asString
+  data: asArray(asEdgeRatesExchangeRate)
 })
 
 function checkIfFiat(code: string): boolean {
@@ -29,30 +59,59 @@ export function makeEdgeRatesPlugin(
 
     async fetchRates(pairsHint) {
       const pairs = []
-      for (const pair of pairsHint) {
-        // Skip if neither code is a fiat code
-        if (!checkIfFiat(pair.fromCurrency) || !checkIfFiat(pair.toCurrency))
-          continue
+      let queryPairs = []
+      const queryArrays = []
+      for (let i = 0; i < pairsHint.length; i++) {
+        queryPairs.push(
+          `${
+            checkIfFiat(pairsHint[i].fromCurrency)
+              ? pairsHint[i].fromCurrency.split(':')[1]
+              : pairsHint[i].fromCurrency
+          }_${
+            checkIfFiat(pairsHint[i].toCurrency)
+              ? pairsHint[i].toCurrency.split(':')[1]
+              : pairsHint[i].toCurrency
+          }`
+        )
 
-        const fiatFrom = pair.fromCurrency.split(':')
-        const fiatTo = pair.toCurrency.split(':')
-        try {
-          const reply = await fetch(
-            `https://rates1.edge.app/v1/exchangeRate?currency_pair=${fiatFrom[1]}_${fiatTo[1]}`
-          )
-          const jsonData = await reply.json()
-          const rate = Number(asEdgeRatesResponse(jsonData).exchangeRate)
-          pairs.push({
-            fromCurrency: pair.fromCurrency,
-            toCurrency: pair.toCurrency,
-            rate
-          })
-        } catch (e) {
-          log.warn(
-            `Issue with EdgeRates rate data structure for ${pair.fromCurrency}/${pair.toCurrency} pair. Error: ${e}`
-          )
+        if (queryPairs.length === 100 || i === pairsHint.length - 1) {
+          queryArrays.push(queryPairs)
+          queryPairs = []
         }
       }
+      for (const query of queryArrays) {
+        const body = JSON.stringify({ pairs: query })
+        try {
+          const reply = await fetch(
+            `https://rates1.edge.app/v1/exchangeRates`,
+            {
+              method: 'POST',
+              body
+            }
+          )
+          const jsonData = asEdgeRatesResponse(await reply.json())
+          for (const exchangeRateObj of Object.keys(jsonData.data)) {
+            try {
+              const rateObj = asEdgeRatesExchangeRate(exchangeRateObj).data
+              if (rateObj.errors) throw new Error(rateObj.errors.message)
+              pairs.push({
+                fromCurrency: rateObj.currency_pair.split('_')[0],
+                toCurrency: rateObj.currency_pair.split('_')[1],
+                rate: Number(rateObj.exchangeRate)
+              })
+            } catch (e) {
+              log.warn(
+                `Issue with EdgeRates rate data structure for ${JSON.stringify(
+                  exchangeRateObj
+                )}. Error: ${e}`
+              )
+            }
+          }
+        } catch (e) {
+          log.warn(`Issue with EdgeRates response data structure. Error: ${e}`)
+        }
+      }
+
       return pairs
     }
   }
