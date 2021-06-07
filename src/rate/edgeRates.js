b8c3eb394a169f7447fdab4161a65b9f7cceb1a9
Matthew <matthew@edge.app>
2021-06-07 00:02:33 -0700

    Use edgeRates for all currencies

--- src/rate/edgeRates.js
+++ src/rate/edgeRates.js
@@ -1,19 +1,20 @@
 // @flow
 
-import { asObject, asString } from 'cleaners'
+import { asArray, asEither, asNull, asObject, asString } from 'cleaners'
 import {
   type EdgeCorePluginOptions,
   type EdgeRatePlugin
 } from 'edge-core-js/types'
 
 const asEdgeRatesResponse = asObject({
-  exchangeRate: asString
+  data: asArray(
+    asObject({
+      currency_pair: asString,
+      date: asString,
+      exchangeRate: asEither(asNull, asString)
+    })
+  )
 })
-
-function checkIfFiat(code: string): boolean {
-  if (code.indexOf('iso:') >= 0) return true
-  return false
-}
 
 export function makeEdgeRatesPlugin(
   opts: EdgeCorePluginOptions
@@ -29,30 +30,41 @@ export function makeEdgeRatesPlugin(
 
     async fetchRates(pairsHint) {
       const pairs = []
-      for (const pair of pairsHint) {
-        // Skip if neither code is a fiat code
-        if (!checkIfFiat(pair.fromCurrency) || !checkIfFiat(pair.toCurrency))
-          continue
 
-        const fiatFrom = pair.fromCurrency.split(':')
-        const fiatTo = pair.toCurrency.split(':')
+      const data = []
+      pairsHint.forEach(pair =>
+        data.push({
+          currency_pair: `${pair.fromCurrency}_${pair.toCurrency}`
+        })
+      )
+
+      while (data.length > 0) {
+        const options = {
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          method: 'POST',
+          body: JSON.stringify({ data: data.splice(0, 100) })
+        }
         try {
           const reply = await fetch(
-            `https://rates1.edge.app/v1/exchangeRate?currency_pair=${fiatFrom[1]}_${fiatTo[1]}`
+            `https://rates1.edge.app/v2/exchangeRates`,
+            options
           )
-          const jsonData = await reply.json()
-          const rate = Number(asEdgeRatesResponse(jsonData).exchangeRate)
-          pairs.push({
-            fromCurrency: pair.fromCurrency,
-            toCurrency: pair.toCurrency,
-            rate
+          const json = asEdgeRatesResponse(await reply.json())
+          json.data.forEach(rate => {
+            if (rate.exchangeRate != null)
+              pairs.push({
+                fromCurrency: rate.currency_pair.split('_')[0],
+                toCurrency: rate.currency_pair.split('_')[1],
+                rate: Number(rate.exchangeRate)
+              })
           })
         } catch (e) {
-          log.warn(
-            `Issue with EdgeRates rate data structure for ${pair.fromCurrency}/${pair.toCurrency} pair. Error: ${e}`
-          )
+          log.warn(`Issue with EdgeRates rate data structure. Error: ${e}`)
         }
       }
+
       return pairs
     }
   }
