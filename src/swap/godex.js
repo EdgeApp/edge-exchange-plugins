1c465e0ad1ab64d6d75814f1f90bf9815ac80b11
Matthew <matthew@edge.app>
2022-01-11 16:39:15 -0800

    Limit AVAX trading to the c-chain

--- src/swap/godex.js
+++ src/swap/godex.js
@@ -75,7 +75,8 @@ const INVALID_CURRENCY_CODES: InvalidCurrencyCodes = {
 
 // Network names that don't match parent network currency code
 const networks = {
-  RBTC: 'RSK'
+  RBTC: 'RSK',
+  AVAX: 'AVAXC'
 }
 
 async function getAddress(wallet: EdgeCurrencyWallet, currencyCode: string) {
