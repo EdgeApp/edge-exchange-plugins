d7be94b4d935c54d5912dc04a9d2420cce903304
Paul Puey <paul@edge.app>
2023-08-18 21:01:21 -0700

    Add a bch wallet for testing

--- test/testpartner.ts
+++ test/testpartner.ts
@@ -8,6 +8,7 @@ import {
 } from 'cleaners'
 import {
   addEdgeCorePlugins,
+  EdgeSwapQuote,
   EdgeSwapRequest,
   lockEdgeCorePlugins,
   makeFakeEdgeWorld
@@ -17,6 +18,7 @@ import fs from 'fs'
 import edgeExchangePlugins from '../src'
 import { arrrCurrencyInfo } from './fakeArrrInfo'
 import { avaxCurrencyInfo } from './fakeAvaxInfo'
+import { bchCurrencyInfo } from './fakeBchInfo'
 import { btcCurrencyInfo } from './fakeBtcInfo'
 import { makeFakePlugin } from './fakeCurrencyPlugin'
 import { ethCurrencyInfo } from './fakeEthInfo'
@@ -41,6 +43,7 @@ const asUserDump = asObject({
 async function main(): Promise<void> {
   const allPlugins = {
     bitcoin: makeFakePlugin(btcCurrencyInfo),
+    bitcoincash: makeFakePlugin(bchCurrencyInfo),
     ethereum: makeFakePlugin(ethCurrencyInfo),
     polygon: makeFakePlugin(polygonCurrencyInfo),
     avalanche: makeFakePlugin(avaxCurrencyInfo),
@@ -64,11 +67,13 @@ async function main(): Promise<void> {
     appId: '',
     plugins: {
       bitcoin: true,
+      bitcoincash: true,
       ethereum: true,
       avalanche: true,
       lifi: true,
       polygon: true,
-      piratechain: true
+      piratechain: true,
+      thorchain: true
       // thorchainda: true,
       // letsexchange: {
       //   apiKey: '',
@@ -79,12 +84,14 @@ async function main(): Promise<void> {
 
   const account = await context.loginWithKey('bob', loginKey)
   const btcInfo = await account.getFirstWalletInfo('wallet:bitcoin')
+  const bchInfo = await account.getFirstWalletInfo('wallet:bitcoincash')
   const ethInfo = await account.getFirstWalletInfo('wallet:ethereum')
   const avaxInfo = await account.getFirstWalletInfo('wallet:avalanche')
   const arrrInfo = await account.getFirstWalletInfo('wallet:piratechain')
   const maticInfo = await account.getFirstWalletInfo('wallet:polygon')
 
   const btcWallet = await account.waitForCurrencyWallet(btcInfo?.id ?? '')
+  const bchWallet = await account.waitForCurrencyWallet(bchInfo?.id ?? '')
   const ethWallet = await account.waitForCurrencyWallet(ethInfo?.id ?? '')
   const avaxWallet = await account.waitForCurrencyWallet(avaxInfo?.id ?? '')
   const arrrWallet = await account.waitForCurrencyWallet(arrrInfo?.id ?? '')
@@ -174,23 +181,134 @@ async function main(): Promise<void> {
     nativeAmount: await maticWallet.denominationToNative('4000', 'USDC'),
     quoteFor: 'from'
   }
-  console.log(`fromRequest:`)
+  console.log(`fromRequest4:`)
   console.log(
     JSON.stringify(
-      { ...fromRequest, fromWallet: undefined, toWallet: undefined },
+      { ...fromRequest4, fromWallet: undefined, toWallet: undefined },
       null,
       2
     )
   )
   console.log(`------------`)
 
-  const quote = await account.fetchSwapQuote(fromRequest4).catch(e => {
+  // Test a FROM quote bitcoincash to ethereum:WBTC
+  const fromRequest5: EdgeSwapRequest = {
+    fromWallet: bchWallet,
+    fromCurrencyCode: 'BCH',
+    toWallet: ethWallet,
+    toCurrencyCode: 'WBTC',
+    nativeAmount: await bchWallet.denominationToNative('10', 'BCH'),
+    quoteFor: 'from'
+  }
+  console.log(`fromRequest5:`)
+  console.log(
+    JSON.stringify(
+      { ...fromRequest5, fromWallet: undefined, toWallet: undefined },
+      null,
+      2
+    )
+  )
+
+  const quote5 = await account.fetchSwapQuote(fromRequest5).catch(e => {
     console.log(e)
     console.log(e.message)
+    return null
   })
-  console.log(JSON.stringify(quote, null, 2))
+  logQuote(quote5)
+  console.log(`------------`)
+
+  // Test a TO quote bitcoincash to ethereum:WBTC using 'to' amount from
+  // previous request
+  const toRequest6: EdgeSwapRequest = {
+    fromWallet: bchWallet,
+    fromCurrencyCode: 'BCH',
+    toWallet: ethWallet,
+    toCurrencyCode: 'WBTC',
+    nativeAmount: quote5?.toNativeAmount ?? '0',
+    // nativeAmount: await ethWallet.denominationToNative('0.15185834', 'WBTC'),
+    quoteFor: 'to'
+  }
+  console.log(`toRequest6:`)
+  console.log(
+    JSON.stringify(
+      { ...toRequest6, fromWallet: undefined, toWallet: undefined },
+      null,
+      2
+    )
+  )
+
+  const quote6 = await account.fetchSwapQuote(toRequest6).catch(e => {
+    console.log(e)
+    console.log(e.message)
+    return null
+  })
+  logQuote(quote6)
   console.log('-------------------------')
+
+  // Test a FROM quote bitcoin to bitcoincash
+  const fromRequest7: EdgeSwapRequest = {
+    fromWallet: btcWallet,
+    fromCurrencyCode: 'BTC',
+    toWallet: bchWallet,
+    toCurrencyCode: 'BCH',
+    nativeAmount: await btcWallet.denominationToNative('0.2', 'BTC'),
+    quoteFor: 'from'
+  }
+  console.log(`fromRequest7:`)
+  console.log(
+    JSON.stringify(
+      { ...fromRequest7, fromWallet: undefined, toWallet: undefined },
+      null,
+      2
+    )
+  )
+
+  const quote7 = await account.fetchSwapQuote(fromRequest7).catch(e => {
+    console.log(e)
+    console.log(e.message)
+    return null
+  })
+  logQuote(quote7)
+  console.log(`------------`)
+
+  // Test a FROM quote bitcoincash to bitcoin
+  const fromRequest8: EdgeSwapRequest = {
+    fromWallet: bchWallet,
+    fromCurrencyCode: 'BCH',
+    toWallet: btcWallet,
+    toCurrencyCode: 'BTC',
+    nativeAmount: await bchWallet.denominationToNative('0.2', 'BCH'),
+    quoteFor: 'from'
+  }
+  console.log(`fromRequest8:`)
+  console.log(
+    JSON.stringify(
+      { ...fromRequest8, fromWallet: undefined, toWallet: undefined },
+      null,
+      2
+    )
+  )
+
+  const quote8 = await account.fetchSwapQuote(fromRequest8).catch(e => {
+    console.log(e)
+    console.log(e.message)
+    return null
+  })
+  logQuote(quote8)
+  console.log(`------------`)
+
   process.exit(0)
+}
+
+const logQuote = (quote: EdgeSwapQuote | null): void => {
+  if (quote == null) {
+    console.log('null quote')
+    return
+  }
+  const loggedQuote = quote as any
+  loggedQuote.request.fromWallet = null
+  loggedQuote.request.toWallet = null
+  console.log(JSON.stringify(loggedQuote, null, 2))
 }
 
 main().catch(e => {
