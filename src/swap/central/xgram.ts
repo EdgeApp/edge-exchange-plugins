cf281ac934c34bb5d561ae2b4031e064953b8f45
xgram <support@xgram.io>
2025-10-30 05:39:44 +0000

    swap refactor, error checker added

--- src/swap/central/xgram.ts
+++ src/swap/central/xgram.ts
@@ -1,5 +1,13 @@
 import { gt, lt } from 'biggystring'
-import { asArray, asBoolean, asObject, asString, Cleaner } from 'cleaners'
+import {
+  asArray,
+  asBoolean,
+  asMaybe,
+  asObject,
+  asOptional,
+  asString,
+  Cleaner
+} from 'cleaners'
 import {
   EdgeCorePluginOptions,
   EdgeMemo,
@@ -9,21 +17,19 @@ import {
   EdgeSwapQuote,
   EdgeSwapRequest,
   SwapAboveLimitError,
-  SwapBelowLimitError,
-  SwapCurrencyError
+  SwapBelowLimitError
 } from 'edge-core-js/types'
 
 import {
   ChainCodeTickerMap,
-  checkInvalidCodes,
   checkWhitelistedMainnetCodes,
   CurrencyPluginIdSwapChainCodeMap,
-  EdgeIdSwapIdMap,
+  denominationToNative,
   ensureInFuture,
   getChainAndTokenCodes,
   getMaxSwappable,
-  InvalidCurrencyCodes,
   makeSwapPluginQuote,
+  nativeToDenomination,
   SwapOrder
 } from '../../util/swapHelpers'
 import { convertRequest, getAddress, memoType } from '../../util/utils'
@@ -39,16 +45,14 @@ export const swapInfo: EdgeSwapInfo = {
 }
 
 const asInitOptions = asObject({
-  apiKey: asString
+  apiKey: asString,
+  affiliateId: asOptional(asString)
 })
 
 const orderUri = 'https://xgram.io/exchange/order?id='
-
 const uri = 'https://xgram.io/api/v1/'
-const INVALID_CURRENCY_CODES: InvalidCurrencyCodes = {
-  from: {},
-  to: {}
-}
+const newExchange = 'launch-new-exchange'
+const newRevExchange = 'launch-new-payment-exchange'
 
 export const MAINNET_CODE_TRANSCRIPTION: CurrencyPluginIdSwapChainCodeMap = {
   algorand: 'ALGO',
@@ -117,22 +121,18 @@ export const MAINNET_CODE_TRANSCRIPTION: CurrencyPluginIdSwapChainCodeMap = {
   sonic: null
 }
 
-export const SPECIAL_MAINNET_CASES: EdgeIdSwapIdMap = new Map([
-  ['avalanche', new Map([[null, { chainCode: 'cchain', tokenCode: 'avax' }]])]
-])
-
 // Provider data
 let chainCodeTickerMap: ChainCodeTickerMap = new Map()
 let lastUpdated = 0
 const EXPIRATION = 1000 * 60 * 60
 
-const swapFix = 'float'
-const swapFloat = 'float'
+const swapType: FlowType = 'fixed'
 type FlowType = 'fixed' | 'float'
 
 export function xgramPlugin(opts: EdgeCorePluginOptions): EdgeSwapPlugin {
   const { io, log } = opts
-  const { fetchCors = io.fetch } = io
+
+  const fetchCors = io.fetch
   const { apiKey } = asInitOptions(opts.initOptions)
 
   const headers = {
@@ -141,20 +141,16 @@ export function xgramPlugin(opts: EdgeCorePluginOptions): EdgeSwapPlugin {
   }
 
   async function fetchSupportedAssets(): Promise<void> {
-    if (lastUpdated > Date.now() - EXPIRATION) return
-
+    if (lastUpdated > Date.now() - EXPIRATION && lastUpdated !== 0) return
     try {
       const response = await fetchCors(`${uri}list-currency-options`, {
         headers
       })
+
       const json = await response.json()
-
-      if (json.result !== true) {
-        throw new Error('Xgram call list currency error')
-      }
-
-      const jsonArr = Object.entries(json).map(([symbol, data]) => ({
-        ...(data as object)
+      const jsonArr = Object.entries(json).map(([key, data]) => ({
+        ...(data as object),
+        coinName: key
       }))
       const assets = asXgramAssets(jsonArr)
 
@@ -165,7 +161,7 @@ export function xgramPlugin(opts: EdgeCorePluginOptions): EdgeSwapPlugin {
           const tokenCodes = out.get(asset.coinName) ?? []
           tokenCodes.push({
             tokenCode: asset.coinName,
-            contractAddress: null // TODO check this val
+            contractAddress: asset.contract
           })
           out.set(asset.network, tokenCodes)
         }
@@ -174,54 +170,34 @@ export function xgramPlugin(opts: EdgeCorePluginOptions): EdgeSwapPlugin {
       chainCodeTickerMap = out
       lastUpdated = Date.now()
     } catch (e) {
-      log.warn('-----> Xgram: Error updating supported assets', e)
+      log.warn('Xgram: Error updating supported assets', e)
     }
   }
 
   const fetchSwapQuoteInner = async (
     request: EdgeSwapRequestPlugin
-    // opts: { promoCode?: string }
   ): Promise<SwapOrder> => {
-    const { nativeAmount } = request
+    const { fromWallet, toWallet, nativeAmount } = request
 
     // Grab addresses:
     const [fromAddress, toAddress] = await Promise.all([
-      getAddress(request.fromWallet),
-      getAddress(request.toWallet)
+      getAddress(fromWallet),
+      getAddress(toWallet)
     ])
 
     const xgramCodes = await getChainAndTokenCodes(
       request,
       swapInfo,
       chainCodeTickerMap,
-      MAINNET_CODE_TRANSCRIPTION,
-      SPECIAL_MAINNET_CASES
+      MAINNET_CODE_TRANSCRIPTION
     )
 
-    const currencyString = `fromCcy=${xgramCodes.fromMainnetCode}&toCcy=${xgramCodes.toCurrencyCode}`
-
     async function createOrder(
-      flow: FlowType,
       isSelling: boolean,
       largeDenomAmount: string
     ): Promise<XgramResponse> {
-      // Get rate
-      let url = `retrieve-rate-value?ccyAmount=${largeDenomAmount}&${currencyString}`
-      if (!isSelling) {
-        const currencySellString = `toCcy=${xgramCodes.fromMainnetCode}&fromCcy=${xgramCodes.toCurrencyCode}`
-        url = `retrieve-rate-value?ccyAmount=${largeDenomAmount}&${currencySellString}`
-      }
-
-      const exchangeAmountResponse = await fetchCors(uri + url, { headers })
-      const exchangeAmountResponseJson = await exchangeAmountResponse.json()
-
-      if (exchangeAmountResponseJson.result !== true)
-        throw new SwapCurrencyError(swapInfo, request)
-
       const validUntil = null
-
-      // Create order
-      const fromCcy = {
+      const orderBody = {
         fromCurrency: xgramCodes.fromCurrencyCode,
         toCurrency: xgramCodes.toCurrencyCode,
         fromNetwork: xgramCodes.fromMainnetCode,
@@ -229,20 +205,21 @@ export function xgramPlugin(opts: EdgeCorePluginOptions): EdgeSwapPlugin {
         fromAmount: isSelling ? largeDenomAmount : '',
         toAmount: isSelling ? '' : largeDenomAmount,
         address: toAddress,
-        refundAddress: fromAddress,
-        flow
-        // payload: { promoCode }
+        refundAddress: fromAddress
       }
+
+      const createExchangeUrl = isSelling ? newExchange : newRevExchange
 
       const orderResponse = await fetchCors(
         uri +
-          'launch-new-exchange' +
+          createExchangeUrl +
           `?${new URLSearchParams({
             toAddress: String(toAddress),
             refundAddress: String(fromAddress),
-            fromCcy: fromCcy.fromCurrency,
-            toCcy: fromCcy.toCurrency,
-            ccyAmount: largeDenomAmount
+            fromCcy: orderBody.fromCurrency,
+            toCcy: orderBody.toCurrency,
+            ccyAmount: largeDenomAmount,
+            type: swapType
           }).toString()}`,
         {
           headers
@@ -251,144 +228,102 @@ export function xgramPlugin(opts: EdgeCorePluginOptions): EdgeSwapPlugin {
 
       const orderResponseJson = await orderResponse.json()
 
+      const quoteFor = request?.quoteFor === 'from' ? 'from' : 'to'
+
       if (orderResponseJson.result !== true) {
-        const errMsg = String(orderResponseJson.error ?? '')
-        throw new Error(`Xgram call returned error message: ${errMsg}`)
+        const denomAmount = denominationToNative(
+          quoteFor === 'from' ? request.fromWallet : request.toWallet,
+          largeDenomAmount,
+          quoteFor === 'from' ? request.fromTokenId : request.toTokenId
+        )
+        if (orderResponseJson.error === 'Min amount error') {
+          throw new SwapBelowLimitError(swapInfo, denomAmount, quoteFor)
+        }
+        if (orderResponseJson.error === 'Max amount error') {
+          throw new SwapAboveLimitError(swapInfo, denomAmount, quoteFor)
+        }
       }
 
       const orderRes = {
         id: orderResponseJson.id,
         validUntil,
-        toAmount: fromCcy.toAmount,
-        fromAmount: fromCcy.fromAmount,
+        fromAmount: isSelling
+          ? orderBody.fromAmount
+          : orderResponseJson.ccyAmountFrom,
+        toAmount: isSelling
+          ? orderResponseJson.ccyAmountToExpected
+          : orderBody.toAmount,
         payinAddress: orderResponseJson.depositAddress,
         payinExtraId: orderResponseJson.depositTag
       }
-
       return orderRes
     }
 
-    async function swapSell(flow: FlowType): Promise<SwapOrder> {
-      const largeDenomAmount = await request.fromWallet.nativeToDenomination(
-        nativeAmount,
-        request.fromCurrencyCode
-      )
+    async function swapExchange(isSelling: boolean): Promise<SwapOrder> {
+      const buyUrl = `fromCcy=${xgramCodes.toCurrencyCode}&toCcy=${xgramCodes.fromMainnetCode}`
+      const sellUrl = `fromCcy=${xgramCodes.fromMainnetCode}&toCcy=${xgramCodes.toCurrencyCode}`
+      const getRateUrl = isSelling ? sellUrl : buyUrl
 
+      const largeDenomAmount = nativeToDenomination(
+        isSelling ? request.fromWallet : request.toWallet,
+        nativeAmount,
+        isSelling ? request.fromTokenId : request.toTokenId
+      )
       // Get min and max
       const marketRangeResponse = await fetchCors(
         uri +
-          `retrieve-rate-value?ccyAmount=${largeDenomAmount}&${currencyString}`,
+          `retrieve-rate-value?ccyAmount=${largeDenomAmount}&${getRateUrl}&type=${swapType}`,
         { headers }
       )
-
       const marketRangeResponseJson = await marketRangeResponse.json()
 
-      if (marketRangeResponseJson.result !== true)
-        throw new SwapCurrencyError(swapInfo, request)
-
       const { minFrom, maxFrom } = asMarketRange(marketRangeResponseJson)
+
       if (lt(largeDenomAmount, minFrom)) {
-        const minNativeAmount = await request.fromWallet.denominationToNative(
-          minFrom.toString(),
-          request.fromCurrencyCode
+        const minNativeAmount = denominationToNative(
+          isSelling ? request.fromWallet : request.toWallet,
+          minFrom,
+          isSelling ? request.fromTokenId : request.toTokenId
         )
-        throw new SwapBelowLimitError(swapInfo, minNativeAmount)
+        if (isSelling) {
+          throw new SwapBelowLimitError(swapInfo, minNativeAmount)
+        } else {
+          throw new SwapBelowLimitError(swapInfo, minNativeAmount, 'to')
+        }
       }
 
-      if (maxFrom != null && gt(largeDenomAmount, maxFrom)) {
-        const maxNativeAmount = await request.fromWallet.denominationToNative(
+      if (gt(largeDenomAmount, maxFrom)) {
+        const maxNativeAmount = denominationToNative(
+          isSelling ? request.fromWallet : request.toWallet,
           maxFrom,
-          request.fromCurrencyCode
+          isSelling ? request.fromTokenId : request.toTokenId
         )
-        throw new SwapAboveLimitError(swapInfo, maxNativeAmount)
+        if (isSelling) {
+          throw new SwapAboveLimitError(swapInfo, maxNativeAmount)
+        } else {
+          throw new SwapAboveLimitError(swapInfo, maxNativeAmount, 'to')
+        }
       }
 
       const {
+        fromAmount,
         toAmount,
         payinAddress,
         payinExtraId,
         id,
         validUntil
-      } = await createOrder(flow, true, largeDenomAmount)
+      } = await createOrder(isSelling, largeDenomAmount)
 
-      const toNativeAmount = await request.toWallet.denominationToNative(
-        toAmount.toString(),
-        request.toCurrencyCode
-      )
-
-      const memos: EdgeMemo[] =
-        payinExtraId == null || payinExtraId === ''
-          ? []
-          : [
-              {
-                type: memoType(request.fromWallet.currencyInfo.pluginId),
-                value: payinExtraId
-              }
-            ]
-
-      const spendInfo: EdgeSpendInfo = {
-        tokenId: request.fromTokenId,
-        spendTargets: [
-          {
-            nativeAmount,
-            publicAddress: payinAddress
-          }
-        ],
-        memos,
-        networkFeeOption: 'high',
-        assetAction: {
-          assetActionType: 'swap'
-        },
-        savedAction: {
-          actionType: 'swap',
-          swapInfo,
-          orderId: id,
-          orderUri: orderUri + id,
-          isEstimate: flow === swapFloat,
-          toAsset: {
-            pluginId: request.toWallet.currencyInfo.pluginId,
-            tokenId: request.toTokenId,
-            nativeAmount: toNativeAmount
-          },
-          fromAsset: {
-            pluginId: request.fromWallet.currencyInfo.pluginId,
-            tokenId: request.fromTokenId,
-            nativeAmount
-          },
-          payoutAddress: toAddress,
-          payoutWalletId: request.toWallet.id,
-          refundAddress: fromAddress
-        }
-      }
-
-      return {
-        request,
-        spendInfo,
-        swapInfo,
-        fromNativeAmount: nativeAmount,
-        expirationDate:
-          validUntil != null
-            ? ensureInFuture(validUntil)
-            : new Date(Date.now() + 1000 * 60)
-      }
-    }
-    async function swapBuy(flow: typeof swapFix): Promise<SwapOrder> {
-      const largeDenomAmount = await request.toWallet.nativeToDenomination(
-        nativeAmount,
-        request.toCurrencyCode
-      )
-
-      const {
-        fromAmount,
-        payinAddress,
-        payinExtraId,
-        id,
-        validUntil
-      } = await createOrder(flow, false, largeDenomAmount)
-
-      const fromNativeAmount = await request.fromWallet.denominationToNative(
+      const fromNativeAmount = denominationToNative(
+        request.fromWallet,
         fromAmount.toString(),
-        request.fromCurrencyCode
+        request.fromTokenId
+      )
+
+      const toNativeAmount = denominationToNative(
+        request.toWallet,
+        toAmount.toString(),
+        request.toTokenId
       )
 
       const memos: EdgeMemo[] =
@@ -423,7 +358,7 @@ export function xgramPlugin(opts: EdgeCorePluginOptions): EdgeSwapPlugin {
           toAsset: {
             pluginId: request.toWallet.currencyInfo.pluginId,
             tokenId: request.toTokenId,
-            nativeAmount: nativeAmount
+            nativeAmount: toNativeAmount
           },
           fromAsset: {
             pluginId: request.fromWallet.currencyInfo.pluginId,
@@ -451,35 +386,27 @@ export function xgramPlugin(opts: EdgeCorePluginOptions): EdgeSwapPlugin {
     const { quoteFor } = request
 
     if (quoteFor === 'from') {
-      return await swapSell(swapFix)
+      return await swapExchange(true)
     } else {
-      return await swapBuy(swapFix)
+      return await swapExchange(false)
     }
   }
 
   const out: EdgeSwapPlugin = {
     swapInfo,
 
-    async fetchSwapQuote(
-      req: EdgeSwapRequest
-      // opts: { promoCode?: string }
-    ): Promise<EdgeSwapQuote> {
+    async fetchSwapQuote(req: EdgeSwapRequest): Promise<EdgeSwapQuote> {
       const request = convertRequest(req)
 
       await fetchSupportedAssets()
 
-      checkInvalidCodes(INVALID_CURRENCY_CODES, request, swapInfo)
       checkWhitelistedMainnetCodes(
         MAINNET_CODE_TRANSCRIPTION,
         request,
         swapInfo
       )
 
-      const newRequest = await getMaxSwappable(
-        fetchSwapQuoteInner,
-        request
-        // opts
-      )
+      const newRequest = await getMaxSwappable(fetchSwapQuoteInner, request)
       const swapOrder = await fetchSwapQuoteInner(newRequest)
       return await makeSwapPluginQuote(swapOrder)
     }
@@ -518,6 +445,7 @@ const asXgramAssets = asArray(
   asObject({
     coinName: asString,
     network: asString,
-    available: asBoolean
+    available: asBoolean,
+    contract: asMaybe(asString, null)
   })
 )
